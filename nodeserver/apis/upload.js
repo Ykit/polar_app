2010895b9396aaa3793f53f6657c18b438c115fa
James <lo.jimmy1994@gmail.com>
2018-07-07 16:01:12 +0800

    Added basic http image services template

---
+++ nodeserver/apis/upload.js
@@ -0,0 +1,7 @@
+function init() {
+  console.log("Initiate image uload");
+}
+
+module.exports = {
+  init
+};
--- nodeserver/server.js
+++ nodeserver/server.js
@@ -2,14 +2,43 @@ const express = require("express");
 
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+const uploadService = require("./apis/upload");
 
 const PORT = "3000";
 const HOST = "127.0.0.1";
 
+/** Statics path */
 const CLIENT = path.resolve(process.cwd(), "dist", "index.html");
+
 app.use(express.static(path.resolve(__dirname, "dist")));
+/** parse application/x-www-form-urlencoded */
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/** parse application/json */
+app.use(bodyParser.json());
+
+/** React frontend root */
 app.get("/", (req, res) => {
   res.sendFile(CLIENT);
+});
+
+/** Computer Vision API
+ * 1. Save to MS object storage
+ * 2. Upload image to MS computer vision api
+ * 3. Process the data from computer vision api
+ * 4. Save to database
+ */
+app.post("/image", (req, res) => {
+  const { payload } = req.body;
+  console.log(payload);
+  uploadService.init();
+  res.json({
+    data: {
+      test: "Hello world",
+      payload
+    }
+  });
 });
 
 app.listen(PORT, () => {
