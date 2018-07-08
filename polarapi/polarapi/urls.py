7988aa90e03035b35e5f159d0b696ed1dc2108b9
Shun Ming YAU <shunmingyau@Shuns-MacBook-Pro.local>
2018-07-08 08:12:13 +0800

    added polarapi

---
+++ polarapi/polarapi/urls.py
@@ -0,0 +1,25 @@
+"""polarapi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import include, path, re_path
+
+urlpatterns = [
+    path(r'recommendation/<str:lat>/<str:longt>/<str:product_name>/<int:product_price>/', include('recommendation.urls')),
+    #re_path(r'recommendation/(?P<lat>[0-9]*)/(?P<long>[0-9]*)/(?P<product_name>[a-zA-Z]*)/(?P<product_price>[0-9]*)/', include('recommendation.urls')),
+    path('admin/', admin.site.urls),
+]
+
